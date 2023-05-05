54da2fa043c9b70c6f7fa30ff772eff399de183c
Mustapha Baryoh <mustapha.baryoh@kellogg.northwestern.edu>
2023-05-05 11:18:04 +0000

    CommitA

--- kmdb.rb
+++ kmdb.rb
@@ -70,6 +70,8 @@
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
 # TODO!
+Movie.destroy_all
+Role.destroy_all
 
 # Generate models and tables, according to the domain model.
 # TODO!
@@ -77,6 +79,28 @@
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
 # TODO!
+
+new_movie = Movie.new
+new_movie["title"] = "Batman Begins"
+new_movie["year_released"] = 2005
+new_movie["rated"] = "PG-13"
+new_movie["studio"] = 1
+new_movie.save
+
+new_movie_2 = Movie.new
+new_movie_2["title"] = "The Dark Knight"
+new_movie_2["year_released"] = 2008
+new_movie_2["rated"] = "PG-13"
+new_movie_2["studio"] = 1
+new_movie_2.save
+
+new_movie_3 = Movie.new
+new_movie_3["title"] = "The Dark Knight Rises"
+new_movie_3["year_released"] = 2012
+new_movie_3["rated"] = "PG-13"
+new_movie_3["studio"] = 1
+new_movie_3.save
+
 
 # Prints a header for the movies output
 puts "Movies"
